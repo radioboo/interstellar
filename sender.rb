bec69e3c586b9b36e14da35ad950442aaa0c5abd
radioboo <sakai.atsushi@gmail.com>
2016-02-06 08:52:58 +0000

    change message fotmat

--- sender.rb
+++ sender.rb
@@ -9,13 +9,35 @@ CONFIG = YAML.load_file('./secrets/secrets.yml')
 SENTINEL_DATE_TXT = 'sentinel_date.txt'
 
 class Slack
-  def self.notify(message)
+  def self.notify(messages)
+    attachments = messages.map do |message|
+      {
+        title: message.title,
+        title_link: message.title_link,
+        text: message.text,
+        color: message.color,
+      }
+    end
+
     RestClient.post CONFIG["slack_url"], {
-      payload:
-      { text: message }.to_json
+      payload: { attachments: attachments }.to_json
     },
     content_type: :json,
     accept: :json
+  end
+end
+
+class Message
+  attr_accessor :title
+  attr_accessor :title_link
+  attr_accessor :text
+  attr_accessor :color
+
+  def initialize(title, title_link, text, color)
+    @title = title
+    @title_link = title_link
+    @text = text
+    @color = color
   end
 end
 
@@ -29,13 +51,13 @@ class Review
   attr_accessor :link
 
   def initialize data = {}
-    @version      = data[:version] ? data[:version].to_s.encode("utf-8") : nil
-    @device       = data[:device] ? data[:device].to_s.encode("utf-8") : nil
+    @version      = data[:version] ? data[:version].to_s.encode("utf-8") : 'バージョンなし'
+    @device       = data[:device] ? data[:device].to_s.encode("utf-8") : 'デバイスなし'
     @submitted_at = DateTime.parse(data[:submitted_at].encode("utf-8"))
     @rate         = data[:rate].encode("utf-8").to_i
-    @title        = data[:title] ? "*#{data[:title].to_s.encode("utf-8")}*\n" : nil
-    @text         = data[:text] ? data[:text].to_s.encode("utf-8") : nil
-    @link         = data[:link].to_s.encode("utf-8")
+    @title        = data[:title] ? "*#{data[:title].to_s.encode("utf-8")}*\n" : 'タイトルなし'
+    @text         = data[:text] ? data[:text].to_s.encode("utf-8") : 'メッセージなし'
+    @link         = data[:link] ? data[:link].to_s.encode("utf-8") : 'https://play.google.com/store/apps/details?id=us.mitene'
   end
 
   def self.collection
@@ -46,35 +68,35 @@ class Review
     selected_message = collection.select do |r|
       r.submitted_at > date && (r.title || r.text)
     end
-    message = selected_message.sort_by do |r|
-      r.submitted_at
-    end.map do |r|
-      r.build_message
-    end.join("\n")
 
-    if message != ""
-      Slack.notify(message)
+    sorted_message = selected_message.sort_by do |r|
+      r.submitted_at
+    end
+
+    messages = sorted_message.map do |r|
+      r.build_message
+    end
+
+    unless messages.empty?
+      Slack.notify(messages)
     else
       print "No new reviews\n"
     end
   end
 
-
-  def notify_to_slack
-    if text || title
-      message = "*Rating: #{rate}* | version: #{version} | subdate: #{submitted_at}\n #{[title, text].join(" ")}\n <#{url}|Ответить в Google play>"
-      Slack.notify(message)
-    end
-  end
-
   def build_message
-    stars = rate.times.map{"★"}.join + (5 - rate).times.map{"☆"}.join
+    color = nil
+    if rate >= 4
+      color = '#36a64f'
+    elsif rate <= 3 && rate > 1
+      color = '#F0B325'
+    else
+      color = '#E5001E'
+    end
 
-    [
-      "\n\n#{stars}",
-      "#{[title, text].join(" ")}",
-      "for v#{version} " " <#{link}| Google Play>",
-    ].join("\n")
+    stars = rate.times.map{"★"}.join + (5 - rate).times.map{"☆"}.join
+    body = "#{stars}\n#{text}\n for v#{version} on #{device} <#{link}| Google Play>"
+    Message.new(title, link, body, color)
   end
 end
 
@@ -88,7 +110,6 @@ csv_file_name = "reviews_#{CONFIG["package_name"]}_#{file_date}.csv"
 system "BOTO_PATH=./secrets/.boto gsutil/gsutil cp -r gs://#{CONFIG["app_repo"]}/reviews/#{csv_file_name} ."
 
 CSV.foreach(csv_file_name, encoding: 'bom|utf-16le', headers: true) do |row|
-  next if row[11].nil?
   Review.collection << Review.new({
     version: row[2],
     device: row[4],
